1c389890c26d5944ffc83cf58a621ab46a76c97d
som <somparashar007@gmail.com>
2026-05-12 18:54:45 +0530

    updated file with query param and http

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res)=>{
 
+    if (req.url === "/favicon.ico") return res.end();
+     
+
     const log = `${Date.now()}: ${req.url} New Req Received\n`
+    const myUrl = url.parse(req.url, true);
+
+    console.log(myUrl);
 
     fs.appendFile("log.txt", log, (err,data)=> {
 
-        switch(req.url) {
-            case '/': res.end("Homepage");
+        switch(myUrl.pathname) {
+            case '/': 
+            res.end("Homepage");
             break;
-            case '/about': res.end(" my name is Som");
+            case '/about': 
+            const username = myUrl.query.myname;
+
+            res.end(`Hi, ${username}`);
             break;
+
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end("Here are your results for" + search);
+
             default:
                 res.end("404 req Not Found");
         }
